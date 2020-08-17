6011af5f672e08ec59acff8ab878e7e9b1ac0aab
wioletsudo <josuelandim@hotmail.com>
2020-08-17 04:46:26 -0300

    blmbgk7

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,27 +1,19 @@
 from imblearn.over_sampling import SMOTE
 from sklearn.base import BaseEstimator, TransformerMixin
 
-	# All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
 	def __init__(self, columns):
-	    self.columns = columns
-
+		self.columns = columns
 	def fit(self, X, y=None):
-	    return self
-
+		return self
 	def transform(self, X):
-	    # Primeiro realizamos a cópia do dataframe 'X' de entrada
-	   	data = X.copy()
-	    # Retornamos um novo dataframe sem as colunas indesejadas
-	    return data.drop(labels=self.columns, axis='columns')
-
+		data = X.copy()
+		return data.drop(labels=self.columns, axis='columns')
 class Smote(BaseEstimator, TransformerMixin):
 	def __init__(self, columns):
-	    self.columns = columns
-
+		self.columns = columns
 	def fit(self, X, y):
 		X, y = SMOTE(random_state=0).fit_resample(X, y)
 		return X, y
-
 	def transform(self, X):
-	    pass
+		pass
